a8607749fff949f0ef209b5c1d399ad8c62edd5b
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-23 16:10:55 +0530

    Update ColorUtils.cpp

--- ColorUtils.cpp
+++ ColorUtils.cpp
@@ -1,13 +1,13 @@
 #include "ColorCoder.h"
-
+using namespace TelCoColorCoder;
  ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor majorColor = 
-            (MajorColor)(zeroBasedPairNumber / TelCoColorCoder::numberOfMinorColors);
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
         MinorColor minorColor =
-            (MinorColor)(zeroBasedPairNumber % TelCoColorCoder::numberOfMinorColors);
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return ColorPair(majorColor, minorColor);
     }
     int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major * TelCoColorCoder::numberOfMinorColors + minor + 1;
+        return major * numberOfMinorColors + minor + 1;
     }
