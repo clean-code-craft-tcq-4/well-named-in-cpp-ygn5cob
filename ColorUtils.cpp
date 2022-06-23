405060faa2705b8f743e2001d77351342112e7f8
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-23 16:34:37 +0530

    Update ColorUtils.cpp

--- ColorUtils.cpp
+++ ColorUtils.cpp
@@ -1,8 +1,5 @@
 #include "ColorCoder.h"
 namespace TelCoColorCoder{
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
     const char* MajorColorNames[] = {
         "White", "Red", "Black", "Yellow", "Violet"
     };
