95f079eeef0b3a8635eaee0de281552a65be434d
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-23 15:53:54 +0530

    Create ColorUtils.cpp

---
+++ ColorUtils.cpp
@@ -0,0 +1,14 @@
+#include "ColorCoder.h"
+
+ ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+}
