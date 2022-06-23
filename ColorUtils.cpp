4387ebe654252404e6b8d10d52e6e90a7ce625f3
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-23 14:43:28 +0530

    Update ColorUtils.cpp

--- ColorUtils.cpp
+++ ColorUtils.cpp
@@ -1,6 +1,20 @@
 #include <assert.h>
 #include "ColorCoder.h"
-
+namespace TelCoColorCoder
+{
+ ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+}
+};
 void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
@@ -21,3 +35,4 @@ void testPairToNumber(
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
 }
+
