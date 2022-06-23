747eff94b90624a653529f2e08e6ce63343c6092
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-23 14:42:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,6 @@
 #include <assert.h>
 #include "ColorCoder.h"
-namespace TelCoColorCoder
-{
- ColorPair GetColorFromPairNumber(int pairNumber) {
-        int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor = 
-            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor =
-            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-        return ColorPair(majorColor, minorColor);
-    }
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major * numberOfMinorColors + minor + 1;
-    }
-}
-};
+
 int main() {
     TelCoColorCoder::ColorPair pair;
     pair.printCombination();
