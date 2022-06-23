8f6ee47d06d173adb8c0bd12bea483bcf7a0e995
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-23 16:31:29 +0530

    Update ColorUtils.cpp

--- ColorUtils.cpp
+++ ColorUtils.cpp
@@ -1,5 +1,38 @@
 #include "ColorCoder.h"
-//using namespace TelCoColorCoder;
+namespace TelCoColorCoder{
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+}
+std::string ToString() {
+                std::string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+            }
+           void printCombination() {
+               int count = 0;
+               std::cout << "pair number\t Major Color\t Minor Color" << std::endl;
+               for(int i=0; i<numberOfMajorColors;i++)
+               {
+                   for(int j=0; j<numberOfMinorColors;j++)
+                   {
+                       count++;
+                       std::cout << count<<"\t\t"<< MajorColorNames[i]<<"\t\t"<<MinorColorNames[j]<< std::endl;
+                   }      
+               }
+           }
+
  TelCoColorCoder::ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         TelCoColorCoder::MajorColor majorColor = 
