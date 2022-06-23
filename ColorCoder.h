f167ef77e61aaabcaf24d62191f658d6488fac53
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-23 16:34:58 +0530

    Update ColorCoder.h

--- ColorCoder.h
+++ ColorCoder.h
@@ -3,6 +3,8 @@
 #include <iostream>
 namespace TelCoColorCoder
 {
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
     class ColorPair {
         private:
             MajorColor majorColor;
