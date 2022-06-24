d854a0221de1f141f909b22c87a8f53ea33ca59e
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-24 10:31:54 +0530

    Update ColorUtil.cpp

--- ColorUtil.cpp
+++ ColorUtil.cpp
@@ -1,5 +1,12 @@
 #include "ColorCoder.h"
 
+namespace TelCoColorCoder {
+
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
  ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor majorColor = 
@@ -11,4 +18,19 @@
     int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
     }
+
+    void printCombination() {
+        int count = 0;
+        std::cout << "pair number\t Major Color\t Minor Color" << std::endl;
+        for(int i=0; i<numberOfMajorColors;i++)
+            {
+            for(int j=0; j<numberOfMinorColors;j++)
+                {
+                    count++;
+                    std::cout << count<<"\t\t"<< MajorColorNames[i]<<"\t\t"<<MinorColorNames[j]<< std::endl;
+                }
+                   
+            }
+    }
 }
+
