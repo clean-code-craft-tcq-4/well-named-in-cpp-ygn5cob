336141efe3e40eca9d08111724ce7117d9a75dd1
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-25 17:50:29 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,13 +11,5 @@ int main() {
     testPairToValue(primaryColor::VIOLET, secondaryColor::SLATE, 25);
 
     // Print Color Code Manual 
-    int userInput;
-    std::cout<<"Press 1 to print Color Code Manual \nPress 0 to exit"<<std::endl;
-    std::cin>>userInput;
-    if(userInput == 1)
         printColorCodeManual();
-    else if(userInput == 0)
-        return 0;
-    else
-        std::cout<<"\nWrong Choice\n";
 }
