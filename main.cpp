4ecee2c86e5982eb53d464c1993847b8adcf382a
ygn5cob <38490766+ygn5cob@users.noreply.github.com>
2022-06-25 17:22:36 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
-#include <assert.h>
-#include "ColorUtil.cpp"
+#include "ColorTest.h"
 using namespace TelCoColorCoder;
 
 int main() {
     
-    printCombination();
-//    testNumberToPair(4, MajorColor::WHITE, MinorColor::BROWN);
-//    testNumberToPair(5, MajorColor::WHITE, MinorColor::SLATE);
+    testNumberToPair(4, MajorColor::WHITE, MinorColor::BROWN);
+    testNumberToPair(5, MajorColor::WHITE, MinorColor::SLATE);
 
-//    testPairToNumber(MajorColor::BLACK, MinorColor::ORANGE, 12);
-//    testPairToNumber(MajorColor::VIOLET, MinorColor::SLATE, 25);
-
+    testPairToNumber(MajorColor::BLACK, MinorColor::ORANGE, 12);
+    testPairToNumber(MajorColor::VIOLET, MinorColor::SLATE, 25);
+	
+	printCombination();
     return 0;
 }
